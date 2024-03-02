bc6b9006f1e745bd1942d17fd4945ace0a972e68
Davidson William <davidsonwferreira8@gmail.com>
2024-03-02 18:26:10 -0300

    feat(Middlewares): Configuração dos middlewares.

--- src/server.js
+++ src/server.js
@@ -2,14 +2,18 @@ import express from "express";
 import { testConnection } from "./config/connection.js";
 import { routes } from "./routes/user.route.js";
 import { dbSync } from "./config/dbSync.js";
+import {
+  corsMiddleware,
+  jsonMiddleware,
+} from "./middlewares/globalMiddlewares.js";
 
 const app = express();
 
-app.use(express.json());
-app.use('/user', routes);
-app.use(dbSync)
+app.use(jsonMiddleware);
+app.use(corsMiddleware);
+app.use("/user", routes);
+app.use(dbSync);
 
 app.listen(3333, () => {
-
   testConnection(), console.log("server on in port 3333!");
 });
